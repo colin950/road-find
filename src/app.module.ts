820273784ba632e19ef2159466645468be4d212b
colin950 <namil9785@naver.com>
2021-10-02 17:52:00 +0900

    conflict resolve

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
-import configuration from './config/configuration';
 import { SnakeNamingStrategy } from 'typeorm-naming-strategies';
 import {AuthModule} from './auth/auth.module'
 import { AwsService } from './aws/aws.service';
