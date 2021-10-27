c5193370b0bd9dcdf824ce9512a801102434b0ef
Seonjae Kim <rhj4862@gmail.com>
2021-10-27 23:01:34 +0900

    fix: CORS 허용 (#21)
    closes #18

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ import { CommonResponseInterceptor } from './util/interceptors/common.response.i
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
